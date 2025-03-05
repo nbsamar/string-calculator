c738a2d5c81b542c553040d8bac8180fdbbff4d1
nbsamar <nbsamar@gmail.com>
2025-03-05 14:39:41 +0530

    Case: When many delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,5 +5,14 @@ class StringCalculator
     return numbers.to_i if numbers.length == 1
 
     numbers.gsub(/\n/, ',').split(',').map(&:to_i).reduce(:+)
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = parts[0][2]
+      numbers = parts[1]
+    end
+
+    numbers.gsub(/\n/, delimiter).split(delimiter).map(&:to_i).reduce(:+)
   end
 end
